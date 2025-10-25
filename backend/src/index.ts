d65f843a3eec40d09ec5600ac4a397ad512dd890
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-10-25 15:18:30 +0300

    initialize index.ts file, nodemon.json file and connect to mongodb

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express(); // initialize express app
+const PORT = 3000; // define port number
+
+//connect to mongodb
+mongoose
+  .connect("mongodb+srv://aymanbajar:asd12341234@cluster0.maovooo.mongodb.net/")
+  .then(() => console.log("MongoDB connected!"))
+  .catch((error) => console.log("failed to connect to MongoDB", error));
+
+// app listen to port
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
