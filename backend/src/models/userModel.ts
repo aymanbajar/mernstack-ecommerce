7c95591262238b55a17af85c97d6e2500277d0b5
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-10-25 16:12:23 +0300

    added comments in userModel file

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,5 +1,7 @@
 import mongoose ,{Schema,Document} from "mongoose";
 
+
+// record schema and model
 export interface IUser extends Document {
     firstName: string;
     lastName: string;
@@ -7,11 +9,13 @@ export interface IUser extends Document {
     password: string;
 }
 
+// create schema
 const  userSchema = new Schema<IUser>({
     firstName :{type:String, required:true},
     lastName :{type:String, required:true},
     email :{type:String, required:true, unique:true},
     password :{type:String, required:true},
 });
+// create model
 export const userModel = mongoose.model<IUser>("User",userSchema);
 
