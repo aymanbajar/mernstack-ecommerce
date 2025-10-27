c945e4512f4de925bf7f339d6fe39b1ceade6651
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-10-27 13:28:58 +0300

    feat(cart): add service for create and get cart for user

---
+++ backend/src/services/cartService.ts
@@ -0,0 +1,34 @@
+import { cartModel } from "../models/cartModel.ts";
+
+// define interface for create  cart for user
+
+interface CreateCartForUser {
+  userId: string;
+}
+
+// define function for create cart  for user
+
+const createCartForUser = async ({ userId }: CreateCartForUser) => {
+// create cart for user
+  const cart = await cartModel.create({ userId });
+  // save cart to database
+  await cart.save();
+  return cart;
+};
+
+//define interface for get active cart for user
+interface GetActiveCartForUser {
+    userId: string;
+}
+
+//define function for get active cart for user
+const getActiveCartForUser = async({ userId }: GetActiveCartForUser) =>{
+    // find active cart for user
+    let cart = await cartModel.findOne({ userId, status: "active" });
+
+    //check if cart exists
+    if (!cart) {
+        cart = await createCartForUser({ userId });
+    }
+    return cart;
+}
