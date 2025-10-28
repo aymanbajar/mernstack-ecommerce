96c479518bf9835c8d9ac235287f2d24811119dc
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-10-28 14:20:47 +0300

    feat(cart): add items to cart route

--- backend/src/routes/cartRoute.ts
+++ backend/src/routes/cartRoute.ts
@@ -23,6 +23,7 @@ router.post('/items',validateJWT,async (req:ExtendRequest,res)=>{
 
  //add items to cart
  const {statusCode,data} = await addItemToCart({userId,productId,quantity});
+ //send response
  res.status(statusCode).send({data});
 
 })
