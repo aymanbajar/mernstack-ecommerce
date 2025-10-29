300fe8b3869263535cf05809eb1576c5e474f4dd
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-10-29 11:48:43 +0300

    feat(cart): implement  update item in cart service

--- backend/src/services/cartService.ts
+++ backend/src/services/cartService.ts
@@ -70,3 +70,43 @@ export const addItemToCart =  async({userId,productId,quantity}:AddItemToCart) =
     return {data:updatedCart,statusCode:200};
 
 }
+
+//define interface for update item in cart
+interface UpdateItemInCart{
+  userId:string;
+  productId:any;
+  quantity:number;
+}
+export const updateItemInCart = async({userId,productId,quantity}:UpdateItemInCart) =>{
+  //get active cart for user
+  const cart = await getActiveCartForUser({userId});
+  //find item in cart
+  const existsInCart = cart.items.find((p) => p.product.toString() === productId);
+  //check if item exists in cart
+  if(!existsInCart){
+    return {data:'product does not exists in cart ',statusCode:404};
+  }
+  //fetch product
+  const product =  await productModel.findById(productId);
+  if(!product){
+    return {data :"Product not found",statusCode:404};
+   }
+  //check if stock low from quantity
+  if(product.stock < quantity){
+    return {data:"low stock for the product",statusCode:400};
+   }
+   const otherCartItems  =cart.items.filter((p) => p.product.toString() !== productId);
+   let total  = otherCartItems.reduce((sum,product) => {
+    return sum + (product.unitPrice * product.quantity);
+   }, 0);
+    // update item quantity in cart
+    existsInCart.quantity = quantity;
+    // update total amount
+    total += existsInCart.unitPrice * quantity;
+    // assign total to cart
+    cart.totalAmount = total;
+    // save cart to database
+    const updatedCart = await cart.save();
+    // return success message
+    return {data:updatedCart,statusCode:200};
+}
