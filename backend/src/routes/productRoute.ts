297e6382d79e93648424d8438ba41569e4b29526
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-10-30 17:54:43 +0300

    refactor(product): add  try-catch for better error handling to  endpoint

--- backend/src/routes/productRoute.ts
+++ backend/src/routes/productRoute.ts
@@ -6,11 +6,15 @@ const router = express.Router();
 
 // get all products endpoint
 router.get("/", async (req, res) => {
-  // get all products from service
+ try{
+    // get all products from service
   const products = await getAllProducts();
 
   // send response products
   res.status(200).send(products);
+ }catch(err){
+  res.status(500).send("something went wrong!");
+ }
 });
 
 export default router;
