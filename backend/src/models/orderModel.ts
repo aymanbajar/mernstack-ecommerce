d1b9c7e76386d730b4348e02252612f146924970
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-10-30 16:55:46 +0300

    feat(order): add order model

---
+++ backend/src/models/orderModel.ts
@@ -0,0 +1,36 @@
+import mongoose, {Schema ,Document, type ObjectId} from 'mongoose';
+
+//define interface for order items
+ export interface IOrderItems {
+    productTitle:string;
+    productImage:string;
+    unitPrice:number;
+    quantity:number;
+}
+
+//define interface for order
+export  interface IOrder extends Document {
+    orderItems: IOrderItems[];
+    total: number;
+    address: string;
+    userId: ObjectId | string;
+}
+
+//create schema for order items
+const orderItemsSchema =  new Schema<IOrderItems>({
+    productTitle: {type: String, required: true},
+    productImage: {type: String, required: true},
+    unitPrice: {type: Number, required: true},
+    quantity: {type: Number, required: true},
+});
+
+//create schema for order
+const orderSchema  =  new Schema<IOrder>({
+    orderItems : [orderItemsSchema],
+    total: {type: Number, required: true},
+    address: {type: String, required: true},
+    userId:{type: Schema.Types.ObjectId, ref: 'User', required: true}
+})    ;
+
+//create order model
+export const OrderModel = mongoose.model<IOrder>('Order', orderSchema);
