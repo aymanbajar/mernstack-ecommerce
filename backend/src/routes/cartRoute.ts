7d4e7f58bc065f1e0b4ab0d218a97d8ff799a6fc
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-10-30 17:52:22 +0300

    refactor(cart): add try-catch for better error handling to endpoints

--- backend/src/routes/cartRoute.ts
+++ backend/src/routes/cartRoute.ts
@@ -13,17 +13,22 @@ const router = express.Router();
 
 // define enddpoint for get active cart for user
 router.get('/',validateJWT, async (req: ExtendRequest ,res) => {
-   // get the userId from the jwt, after vailditing from middleware.
+  try{
+      // get the userId from the jwt, after vailditing from middleware.
    const userId = req.user?.id;
     //get active cart for user
     const cart = await getActiveCartForUser({ userId });
     res.status(200).send(cart);
+  }catch(err){
+    res.status(500).send("something went wrong!");
+  }
 })
 
 //Add items to cart endpoint
 
 router.post('/items',validateJWT,async (req:ExtendRequest,res)=>{
-  const userId =  req.user?._id;
+  try{
+    const userId =  req.user?._id;
  const {productId ,quantity} = req.body;
 
  //add items to cart
@@ -31,40 +36,56 @@ router.post('/items',validateJWT,async (req:ExtendRequest,res)=>{
  //send response
  res.status(statusCode).send({data});
 
+  }catch(err){
+    res.status(500).send("something went wrong!");
+  }
 })
 
 // update item in cart endpoint
 router.put('/items',validateJWT,async (req:ExtendRequest,res)=>{
-  //get user id from jwt
+  try{
+     //get user id from jwt
   const userId =  req.user?._id;
   // get product id and quantity from body
   const {productId ,quantity} = req.body;
   const {data ,statusCode} = await updateItemInCart({userId,productId,quantity});
   res.status(statusCode).send({data});
+  }catch(err){
+    res.status(500).send("something went wrong!");
+  }
 
 })
 
 //define endpoint for delete item from cart
 router.delete('/items/:productId',validateJWT,async (req:ExtendRequest,res)=>{
-  //get user id from jwt
+  try{
+      //get user id from jwt
   const userId =  req.user?._id;
   // get product id from params
   const {productId} = req.params;
   const {data ,statusCode} = await deleteItemInCart({userId,productId});
   res.status(statusCode).send({data});
+  }catch(err){
+    res.status(500).send("something went wrong!");
+  }
 })
 
 //define endpoint for clear cart
 router.delete('/',validateJWT,async (req:ExtendRequest,res)=>{
-  //get user id from jwt
+try{
+    //get user id from jwt
   const userId =  req.user?._id;
   const {data ,statusCode} = await clearCart({userId});
   res.status(statusCode).send({data});
+}catch(err){
+  res.status(500).send("something went wrong!");
+}
 })
 
 //define endpoint for checkout cart
 router.post('/checkout',validateJWT, async (req:ExtendRequest,res)=>{
-    //get user id from jwt
+  try{
+      //get user id from jwt
     const userId = req.user?._id;
     //get addres from body
     const {address} = req.body;
@@ -72,6 +93,9 @@ router.post('/checkout',validateJWT, async (req:ExtendRequest,res)=>{
     const {data ,statusCode} = await checkout({userId,address});
     //send response
     res.status(statusCode).send({data});
+  }catch(err){
+    res.status(500).send("something went wrong!");
+  }
 
 
 });
