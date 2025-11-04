0b0919d298be2ccbfd0b4299b62f9c11fda42b32
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-11-04 19:07:45 +0300

    feat(app): use auth provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,31 @@
-import {BrowserRouter, Route ,Routes} from  'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import Footer from './components/Footer';
-import RegisterPage from './pages/RegisterPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import Footer from "./components/Footer";
+import RegisterPage from "./pages/RegisterPage";
+import { AuthProvider } from "./contexts/Auth/AuthProvider";
 function App() {
-
-
   return (
     <>
-
-    {/* browser router */}
+      <AuthProvider>
+      {/* browser router */}
       <BrowserRouter>
         {/* navbar */}
         <Navbar />
-        
+
         {/* routes */}
         <Routes>
           {/* Route paths */}
-          <Route path='/' element={ <HomePage /> } />
-          <Route path='/register' element={ <RegisterPage /> } />
-
-        
- 
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
         {/* footer */}
         <Footer />
       </BrowserRouter>
-
+      </AuthProvider>
     </>
-  )
+  );
 }
 
 export default App;
