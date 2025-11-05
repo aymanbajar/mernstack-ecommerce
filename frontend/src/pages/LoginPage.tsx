95a1c12e3f04ff271277fa1deed24292d8c3d42b
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-11-05 07:26:39 +0300

    feat(pages): add login page with structure

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,25 @@
+export default function LoginPage() {
+
+    return (
+        <div>
+            <h1>Login to your account</h1>
+
+        <form >
+        <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+        </div>
+
+        <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Login</button>
+
+        </form>
+
+
+
+        </div>
+    )
+}
