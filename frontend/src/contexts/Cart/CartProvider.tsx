0740b1adc2b41aa09daeb2960f2b66edf8d61cc7
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-11-09 11:48:06 +0300

    feat(contexts): implement add item to cart in cart provider

--- frontend/src/contexts/Cart/CartProvider.tsx
+++ frontend/src/contexts/Cart/CartProvider.tsx
@@ -1,16 +1,51 @@
 import { type FC, type PropsWithChildren, useState } from "react";
 import { CartContext } from "./CartContext";
+import axios from "axios";
 import type { CartItem } from "../../types/CartItem";
-export const CartProvider :FC<PropsWithChildren> = ({ children }) => {
-    const [cartItems, setCartItems] = useState<CartItem[]>([]);
-    const [totalAmount, setTotalAmount] = useState<number>(0);
-    const addItemToCart = (productId: string) => {
-        console.log(`Adding product with ID: ${productId} to cart`);
+import { BASE_URL } from "../../constants/BASE_URL";
+import { useAuth } from "../Auth/AuthContext";
+export const CartProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [totalAmount, setTotalAmount] = useState<number>(0);
+  const { token } = useAuth();
+  const [error, setError] = useState<string | null>(null);
+  const addItemToCart = async (productId: string) => {
+    try {
+      const response = await axios.post(
+        `${BASE_URL}/cart/items`,
+        {
+          productId,
+          quantity: 1,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (!response.data) {
+        setError("Failed to add item to cart");
+        return;
+      }
+      const cart = await response.data;
+      const cartItemsMapped = cart.items.map(({ product, quantity }: { product: any; quantity: number }) => ({
+        productId: product._id,
+        title: product.title,
+        image: product.image,
+        quantity,
+        unitPrice: product.price,
+      }));
+      setCartItems([...cartItemsMapped]);
+      setTotalAmount(cart.totalAmount);
+    } catch (err) {
+      console.error("Failed to add item to cart:", err);
     }
+  };
 
-    return(
-        <CartContext.Provider value ={{ cartItems, totalAmount, addItemToCart }}>
-            {children}
-        </CartContext.Provider>
-    )
-}
+  return (
+    <CartContext.Provider value={{ cartItems, totalAmount, addItemToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
